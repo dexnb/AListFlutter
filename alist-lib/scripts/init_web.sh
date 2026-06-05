92896987b2c5d86788e09618d6a3e209ca8371dd
dexnb <138100345+dexnb@users.noreply.github.com>
2026-06-05 23:41:55 +0800

    Update init_web.sh

--- alist-lib/scripts/init_web.sh
+++ alist-lib/scripts/init_web.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -L https://github.com/alist-org/alist-web/releases/latest/download/dist.tar.gz -o dist.tar.gz
+curl -L https://github.com/OpenListTeam/OpenList-Frontend/releases/download/v4.2.2/openlist-frontend-dist-v4.2.2.tar.gz -o dist.tar.gz
 tar -zxvf dist.tar.gz
 rm -rf ../public/dist
 mv -f dist ../public
